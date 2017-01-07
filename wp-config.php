04522192e13ec9751e3f546c0c8ddbe249fcb4ab
Lê Trọng Nghĩa <g.bavuongctk7@gmail.com>
2017-01-07 12:14:38 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'vieclamsv');
+define('DB_NAME', 'u299971761_vlsv');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'u299971761_vlsv');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'vertrigo');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.hostinger.vn');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
